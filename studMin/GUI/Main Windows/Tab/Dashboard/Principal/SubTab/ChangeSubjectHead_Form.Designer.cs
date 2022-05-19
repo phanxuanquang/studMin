3e6e678db12accb7b1649d04760b2f26382967bb
Phan Xuân Quang <20521008@ms.uit.edu.vn>
2022-05-19 23:15:10 +0700

    add change subject head form

---
+++ studMin/GUI/Main Windows/Tab/Dashboard/Principal/SubTab/ChangeSubjectHead_Form.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace studMin
+{
+    partial class ChangeSubjectHead_Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Exit_Button = new Guna.UI2.WinForms.Guna2Button();
+            this.Confirm_Button = new Guna.UI2.WinForms.Guna2Button();
+            this.SubjectName_Label = new System.Windows.Forms.Label();
+            this.ShadowForm = new Guna.UI2.WinForms.Guna2ShadowForm(this.components);
+            this.guna2Elipse1 = new Guna.UI2.WinForms.Guna2Elipse(this.components);
+            this.SubjectTeacher_ComboBox = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.SuspendLayout();
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Exit_Button.Animated = true;
+            this.Exit_Button.BackColor = System.Drawing.Color.Transparent;
+            this.Exit_Button.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.Exit_Button.BorderRadius = 10;
+            this.Exit_Button.CheckedState.Parent = this.Exit_Button;
+            this.Exit_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Exit_Button.CustomImages.Parent = this.Exit_Button;
+            this.Exit_Button.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.Exit_Button.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Exit_Button.ForeColor = System.Drawing.Color.White;
+            this.Exit_Button.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(190)))), ((int)(((byte)(250)))));
+            this.Exit_Button.HoverState.Parent = this.Exit_Button;
+            this.Exit_Button.Location = new System.Drawing.Point(235, 121);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.ShadowDecoration.Parent = this.Exit_Button;
+            this.Exit_Button.Size = new System.Drawing.Size(143, 36);
+            this.Exit_Button.TabIndex = 130;
+            this.Exit_Button.Text = "THOÁT";
+            this.Exit_Button.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            this.Exit_Button.Click += new System.EventHandler(this.Exit_Button_Click);
+            // 
+            // Confirm_Button
+            // 
+            this.Confirm_Button.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Confirm_Button.Animated = true;
+            this.Confirm_Button.BackColor = System.Drawing.Color.White;
+            this.Confirm_Button.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.Confirm_Button.BorderRadius = 10;
+            this.Confirm_Button.BorderThickness = 2;
+            this.Confirm_Button.CheckedState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(210)))), ((int)(((byte)(225)))), ((int)(((byte)(249)))));
+            this.Confirm_Button.CheckedState.Parent = this.Confirm_Button;
+            this.Confirm_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Confirm_Button.CustomImages.Parent = this.Confirm_Button;
+            this.Confirm_Button.FillColor = System.Drawing.Color.White;
+            this.Confirm_Button.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.Confirm_Button.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.Confirm_Button.HoverState.FillColor = System.Drawing.Color.White;
+            this.Confirm_Button.HoverState.Parent = this.Confirm_Button;
+            this.Confirm_Button.ImageAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.Confirm_Button.Location = new System.Drawing.Point(57, 121);
+            this.Confirm_Button.Name = "Confirm_Button";
+            this.Confirm_Button.PressedDepth = 20;
+            this.Confirm_Button.ShadowDecoration.Parent = this.Confirm_Button;
+            this.Confirm_Button.Size = new System.Drawing.Size(143, 36);
+            this.Confirm_Button.TabIndex = 129;
+            this.Confirm_Button.Text = "XÁC NHẬN";
+            this.Confirm_Button.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            // 
+            // SubjectName_Label
+            // 
+            this.SubjectName_Label.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.SubjectName_Label.Dock = System.Windows.Forms.DockStyle.Top;
+            this.SubjectName_Label.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.SubjectName_Label.ForeColor = System.Drawing.Color.White;
+            this.SubjectName_Label.Location = new System.Drawing.Point(0, 0);
+            this.SubjectName_Label.Name = "SubjectName_Label";
+            this.SubjectName_Label.Size = new System.Drawing.Size(434, 44);
+            this.SubjectName_Label.TabIndex = 131;
+            this.SubjectName_Label.Text = "THAY ĐỔI TRƯỞNG BỘ MÔN";
+            this.SubjectName_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.SubjectName_Label.UseCompatibleTextRendering = true;
+            // 
+            // guna2Elipse1
+            // 
+            this.guna2Elipse1.BorderRadius = 10;
+            this.guna2Elipse1.TargetControl = this;
+            // 
+            // SubjectTeacher_ComboBox
+            // 
+            this.SubjectTeacher_ComboBox.Animated = true;
+            this.SubjectTeacher_ComboBox.BackColor = System.Drawing.Color.Transparent;
+            this.SubjectTeacher_ComboBox.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(140)))), ((int)(((byte)(190)))), ((int)(((byte)(250)))));
+            this.SubjectTeacher_ComboBox.BorderRadius = 5;
+            this.SubjectTeacher_ComboBox.BorderThickness = 2;
+            this.SubjectTeacher_ComboBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SubjectTeacher_ComboBox.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.SubjectTeacher_ComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SubjectTeacher_ComboBox.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.SubjectTeacher_ComboBox.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.SubjectTeacher_ComboBox.FocusedState.Parent = this.SubjectTeacher_ComboBox;
+            this.SubjectTeacher_ComboBox.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.SubjectTeacher_ComboBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(88)))), ((int)(((byte)(112)))));
+            this.SubjectTeacher_ComboBox.FormattingEnabled = true;
+            this.SubjectTeacher_ComboBox.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(118)))), ((int)(((byte)(245)))));
+            this.SubjectTeacher_ComboBox.HoverState.Parent = this.SubjectTeacher_ComboBox;
+            this.SubjectTeacher_ComboBox.ItemHeight = 30;
+            this.SubjectTeacher_ComboBox.Items.AddRange(new object[] {
+            "Chọn giáo viên trưởng bộ môn mới"});
+            this.SubjectTeacher_ComboBox.ItemsAppearance.Parent = this.SubjectTeacher_ComboBox;
+            this.SubjectTeacher_ComboBox.Location = new System.Drawing.Point(57, 66);
+            this.SubjectTeacher_ComboBox.Name = "SubjectTeacher_ComboBox";
+            this.SubjectTeacher_ComboBox.ShadowDecoration.Parent = this.SubjectTeacher_ComboBox;
+            this.SubjectTeacher_ComboBox.Size = new System.Drawing.Size(321, 36);
+            this.SubjectTeacher_ComboBox.StartIndex = 0;
+            this.SubjectTeacher_ComboBox.TabIndex = 132;
+            this.SubjectTeacher_ComboBox.TextOffset = new System.Drawing.Point(5, 0);
+            // 
+            // ChangeSubjectHead_Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(434, 172);
+            this.Controls.Add(this.SubjectTeacher_ComboBox);
+            this.Controls.Add(this.SubjectName_Label);
+            this.Controls.Add(this.Exit_Button);
+            this.Controls.Add(this.Confirm_Button);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "ChangeSubjectHead_Form";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ChangeSubjectName_Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button Exit_Button;
+        protected Guna.UI2.WinForms.Guna2Button Confirm_Button;
+        private System.Windows.Forms.Label SubjectName_Label;
+        private Guna.UI2.WinForms.Guna2ShadowForm ShadowForm;
+        private Guna.UI2.WinForms.Guna2Elipse guna2Elipse1;
+        private Guna.UI2.WinForms.Guna2ComboBox SubjectTeacher_ComboBox;
+    }
+}
---
+++ studMin/GUI/Main Windows/Tab/Dashboard/Principal/SubTab/ChangeSubjectHead_Form.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studMin
+{
+    public partial class ChangeSubjectHead_Form : Form
+    {
+        public ChangeSubjectHead_Form()
+        {
+            InitializeComponent();
+            ShadowForm.SetShadowForm(this);
+        }
+
+        private void Exit_Button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
