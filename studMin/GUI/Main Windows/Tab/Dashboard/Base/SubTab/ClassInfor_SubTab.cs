a5e23891624a476e063be7e2e9d235e5bd2e7f1a
TranDinhKhoi2002 <87485108+TranDinhKhoi2002@users.noreply.github.com>
2022-06-04 11:34:07 +0700

    fix logic xuất và filter Danh sách học sinh

--- studMin/GUI/Main Windows/Tab/Dashboard/Base/SubTab/ClassInfor_SubTab.cs
+++ studMin/GUI/Main Windows/Tab/Dashboard/Base/SubTab/ClassInfor_SubTab.cs
@@ -267,16 +267,9 @@ namespace studMin
             DataTable_Info.Columns.Add("Column6", "Số điện thoại");
             DataTable_Info.Columns.Add("Column7", "Email");
 
-            List<STUDENT> listStudents = new List<STUDENT>();
-
-            foreach (var classItem in listClasses)
-            {
-                if (classItem.CLASSNAME == Class_ComboBox.SelectedItem.ToString() && classItem.SCHOOLYEAR == SchoolYear_ComboBox.SelectedItem.ToString())
-                {
-                    listStudents = classItem.STUDENTs.ToList();
-                }
-            }
-
+            string className = Class_ComboBox.SelectedItem.ToString();
+            string schoolYear = SchoolYear_ComboBox.SelectedItem.ToString();
+            List<STUDENT> listStudents = ClassServices.Instance.GetListStudentOfClass(className, schoolYear);
             foreach (var student in listStudents)
             {
                 string sex = student.INFOR.SEX == 0 ? "Nam" : "Nữ";
